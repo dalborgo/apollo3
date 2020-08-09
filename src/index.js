1c1d44292b41ffb7a82458fbe0f1d61f87b98daf
dalborgo <Montebaldo1>
2020-08-09 16:59:22 +0200

    third

--- src/index.js
+++ src/index.js
@@ -20,35 +20,59 @@ const cache = new InMemoryCache({
 const client = new ApolloClient({ cache })
 
 const newDog = {
-  id: '6',
+  id: 6,
   breed: 'lucky',
   displayImage: 'miao',
   __typename: 'Dog',
 }
+const newDog2 = {
+  id: 7,
+  breed: 'lucky2',
+  displayImage: 'miao2',
+  __typename: 'Dog',
+}
 
 const GET_ALL_DOGS = gql`
-    query GetAllDogs {
-        dogs {
-            id
-            breed
-            displayImage
-        }
+  query GetAllDogs {
+    dogs {
+      id
+      breed
+      displayImage
     }
+  }
 `
 const GET_DOG = gql`
-    query GetAllDogs {
-        dog(id: 6) {
-            id
-            breed
-            displayImage
-        }
+  query GetAllDogs {
+    dog(id: 7) {
+      id
+      breed
+      displayImage
     }
+  }
 `
 
 client.writeQuery({
   query: GET_ALL_DOGS,
   data: {
-    dogs: [newDog],
+    dogs: [newDog, newDog2],
+  },
+})
+
+const MY_FRAG = gql`
+  fragment MyDog on Dog {
+    id
+    breed
+    displayImage
+  }
+`
+client.writeFragment({
+  id: '9',
+  fragment: MY_FRAG,
+  data: {
+    id: 9,
+    breed: 'sec',
+    displayImage: 'mio',
+    __typename: 'Dog',
   },
 })
 
@@ -56,6 +80,12 @@ const MyApp = () => {
   const { dogs } = client.readQuery({ query: GET_ALL_DOGS })
   const { dog } = client.readQuery({ query: GET_DOG })
   console.log('dog:', dog)
+  
+  const dog2 = client.readFragment({
+    id: 9,
+    fragment: MY_FRAG,
+  })
+  console.log('dog2:', dog2)
   return dogs.map(({ breed, displayImage }) => (
     <div key={breed}>
       <p>
