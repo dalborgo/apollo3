7d147c64b02d58c084b762aeb8776763cc3805e3
dalborgo <marco@dalborgo.it>
2020-08-12 18:30:26 +0200

    fourth

--- src/index.js
+++ src/index.js
@@ -1,26 +1,37 @@
-import React from 'react'
+import React, { memo, useReducer, useState } from 'react'
 import ReactDOM from 'react-dom'
-import { ApolloClient, ApolloProvider, gql, InMemoryCache, useQuery } from '@apollo/client'
+import { ApolloClient, ApolloProvider, gql, InMemoryCache, makeVar, useQuery } from '@apollo/client'
+import './wdyr'
 
+const cartItemsVar = makeVar([])
 const typeDefs = gql`
   directive @client on FIELD_DEFINITION
   extend type Query {
     dogs: [Dog]
     dog(id: ID!): Dog
+    cartItems: [String]
   }
   type Dog {
     id: ID!
     breed: String
     displayImage: String
   }
-  extend type Product {
-    isInCart: Boolean
+  type Product {
+    isInCart: Int
   }
 `
 
-
 const cache = new InMemoryCache({
   typePolicies: {
+    Product: {
+      fields: { // Field policy map for the Product type
+        isInCart: { // Field policy for the isInCart field
+          read (_, { variables }) { // The read function for the isInCart field
+            return variables.id * 2
+          },
+        },
+      },
+    },
     Query: {
       fields: {
         dog (_, { args, toReference }) {
@@ -29,10 +40,15 @@ const cache = new InMemoryCache({
             id: args.id,
           })
         },
-        isInCart: { // Field policy for the isInCart field
-          read () { // The read function for the isInCart field
-            return true
+        cartItems: {
+          read () {
+            return cartItemsVar()
           },
+        },
+      },
+      cartItems: {
+        read () {
+          return cartItemsVar()
         },
       },
     },
@@ -83,6 +99,12 @@ const GET_PRODUCT_DETAILS = gql`
   }
 `
 
+export const GET_CART_ITEMS = gql`
+  query GetCartItems {
+    cartItems @client
+  }
+`
+
 client.writeQuery({
   query: GET_ALL_DOGS,
   data: {
@@ -108,11 +130,88 @@ client.writeFragment({
   },
 })
 
+// eslint-disable-next-line react/display-name
+const AddToCartButtonWhy = () => {
+  const cartItems = cartItemsVar()
+  console.log('%cRENDER_BUTTON', 'color: orange')
+  return (
+    <div>
+      <button
+        onClick={() => cartItemsVar([...cartItems, String(new Date())])}
+      >
+        Add to Cart
+      </button>
+    </div>
+  )
+}
+AddToCartButtonWhy.whyDidYouRender = true
+// eslint-disable-next-line react/display-name
+const AddToCartButton2Why = memo(({ dispatch }) => {
+  console.log('%cRENDER_BUTTON2', 'color: pink')
+  return (
+    <div>
+      <button
+        onClick={() => dispatch({ type: 'setVal', payload: String(new Date()) })}
+      >
+        Add to Cart 2
+      </button>
+    </div>
+  )
+})
+
+AddToCartButton2Why.whyDidYouRender = true
+
+function reducer (state, action) {
+  switch (action.type) {
+    case 'setVal':
+      return [...state, action.payload]
+    default:
+      return state
+  }
+}
+
+function Cart () {
+  const [render, setRender] = useState('')
+  const { data, loading, error } = useQuery(GET_CART_ITEMS)
+  const [state, dispatch] = useReducer(reducer, [])
+  console.log('state:', state)
+  console.log('render:', render)
+  if (loading) {
+    return <span>Loading...</span>
+  }
+  if (error) {
+    return <p>ERROR: {error.message}</p>
+  }
+  return (
+    <div className="cart">
+      <div>My Cart</div>
+      <AddToCartButtonWhy/>
+      <AddToCartButton2Why dispatch={dispatch}/>
+      <button onClick={() => setRender(new Date())}>Render</button>
+      {
+        data && data.cartItems.length === 0 ? (
+          <p>No items in your cart</p>
+        ) : (
+          <>
+            {
+              data && data.cartItems.map(productId => (
+                <div key={productId}>{productId}</div>
+              ))
+            }
+          </>
+        )
+      }
+    </div>
+  )
+}
+
+// eslint-disable-next-line no-unused-vars
 const MyApp = () => {
+  console.log('%cRENDER_COMP', 'color: yellow')
   const { dogs } = client.readQuery({ query: GET_ALL_DOGS })
   const { dog } = client.readQuery({ query: GET_DOG, variables: { id: 7 } })
   const { data } = useQuery(GET_PRODUCT_DETAILS, { variables: { id: 12 } })
-  //console.log('dog:', dog)
+  console.log('dog:', dog)
   console.log('data:', data)
   const dog2 = client.readFragment({
     id: 9,
@@ -131,14 +230,9 @@ const MyApp = () => {
 function App () {
   return (
     <ApolloProvider client={client}>
-      <MyApp/>
+      <Cart/>
     </ApolloProvider>
   )
 }
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App/>
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+ReactDOM.render(<App/>, document.getElementById('root'))
