adb94d2251307ea752cf03cc3925292752f55bfb
dalborgo <Montebaldo1>
2020-08-16 17:37:23 +0200

    query

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,15 @@
+/*const { override } = require('customize-cra')
+const { addReactRefresh } = require('customize-cra-react-refresh')
+*/
 const rewireInlineImportGraphqlAst = require('react-app-rewire-inline-import-graphql-ast')
 
 module.exports = function override (config, env) {
   config = rewireInlineImportGraphqlAst(config, env)
   return config
 }
+/*
+module.exports = override(
+  addReactRefresh()
+)
 
-
-
+*/
